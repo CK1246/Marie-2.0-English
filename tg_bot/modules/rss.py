09870241f28550c75f288ab55a72cd702a606351
CK1246 <63644027+CK1246@users.noreply.github.com>
2020-07-19 22:29:27 +0530

    Update rss.py

--- tg_bot/modules/rss.py
+++ tg_bot/modules/rss.py
@@ -18,14 +18,7 @@ def show_url(bot, update, args):
         link_processed = parse(tg_feed_link)
 
         if link_processed.bozo == 0:
-            feed_title = link_processed.feed.get("title", default="Unknown")
-            feed_description = "<i>{}</i>".format(
-                re.sub('<[^<]+?>', '', link_processed.feed.get("description", default="Unknown")))
-            feed_link = link_processed.feed.get("link", default="Unknown")
-
-            feed_message = "\n\n<b>Feed Link:</b> \n{}".format(html.escape(feed_title),
-                                                               feed_description,
-                                                               html.escape(feed_link))
+            
 
             if len(link_processed.entries) >= 1:
                 entry_title = link_processed.entries[0].get("title", default="Unknown")
@@ -33,11 +26,9 @@ def show_url(bot, update, args):
                     re.sub('<[^<]+?>', '', link_processed.entries[0].get("description", default="Unknown")))
                 entry_link = link_processed.entries[0].get("link", default="Unknown")
 
-                entry_message = "\n\n<b>Entry Title:</b> \n{}" \
-                                "\n\n<b>Entry Description:</b> \n{}" \
-                                "\n\n<b>Entry Link:</b> \n{}".format(html.escape(entry_title),
-                                                                     entry_description,
-                                                                     html.escape(entry_link))
+                entry_message = "\n\n<b>Title:</b> \n{}" \
+                                "\n\n<b> Link:</b> \n{}".format(html.escape(entry_title),
+                                                               html.escape(entry_link))
                 final_message = feed_message + entry_message
 
                 bot.send_message(chat_id=tg_chat_id, text=final_message, parse_mode=ParseMode.HTML)
